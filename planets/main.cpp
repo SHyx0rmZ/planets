eb0403a8a84908f4094a2ce00df42bd082ae1320
Patrick Pokatilo <mail@shyxormz.net>
2013-03-29 07:03:06 +0100

    Create window

--- planets/main.cpp
+++ planets/main.cpp
@@ -1,11 +1,106 @@
 #include <iostream>
 #include <cstdlib>
+#include <stdexcept>
+#include <Windows.h>
 
 using namespace std;
 
+LRESULT _stdcall application_loop(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+        break;
+
+    case WM_PAINT:
+        PAINTSTRUCT ps;
+
+        BeginPaint(window, &ps);
+        EndPaint(window, &ps);
+        break;
+
+    default:
+        return DefWindowProc(window, message, wparam, lparam);
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
+    int exit = EXIT_SUCCESS;
+
+    /* Window */
+    HINSTANCE instance;
+    ATOM atom;
+    HWND window;
+    unsigned long int style = WS_POPUP;
+    unsigned long int exstyle = WS_EX_APPWINDOW;
+
+    /* Create window */
+    instance = GetModuleHandle(NULL);
+
+    if (instance == NULL)
+    {
+        cerr << "Could not retrieve module handle" << endl;
+
+        return EXIT_FAILURE;
+    }
+
+    WNDCLASSEX wc;
+
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_CLASSDC | CS_HREDRAW | CS_VREDRAW | CS_GLOBALCLASS;
+    wc.lpfnWndProc = application_loop;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = DLGWINDOWEXTRA;
+    wc.hInstance = instance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+    wc.lpszMenuName = 0;
+    wc.lpszClassName = "planets 3d window";
+
+    atom = RegisterClassEx(&wc);
+
+    RECT view;
+
+    view.left = 0;
+    view.top = 0;
+    view.right = GetSystemMetrics(SM_CXSCREEN);
+    view.bottom = GetSystemMetrics(SM_CYSCREEN);
+
+    if (AdjustWindowRectEx(&view, style, FALSE, exstyle) == 0)
+    {
+        cerr << "Could not adjust window rectangle" << endl;
+    }
+
+    window = CreateWindowEx(exstyle, "planets 3d window", "OpenGL planets", style, view.left, view.top, view.right - view.left, view.bottom - view.top, NULL, NULL, instance, NULL);
+
+    if (window == 0)
+    {
+        cerr << "Could not create window" << endl;
+
+        exit = EXIT_FAILURE;
+
+        goto cleanup_window;
+    }
+
+    ShowWindow(window, SW_SHOW);
+    UpdateWindow(window);
+
+    /* Do stuff */
+    /* ... */
+
+cleanup_window:
+    /* Destroy window */
+    ShowWindow(window, SW_HIDE);
+    DestroyWindow(window);
+    UnregisterClass(reinterpret_cast<LPCSTR>(atom), instance);
+
     cout << "Shutting down" << endl;
 
-    return EXIT_SUCCESS;
+    return exit;
 }
